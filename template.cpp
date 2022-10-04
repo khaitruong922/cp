f450deaa9f0d41fb88f2bf26c5fc45680b73eb83
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-10-04 19:57:32 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 template <typename A, typename B>
 ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
 template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
 ostream &operator<<(ostream &os, const T_container &v)
 {
@@ -15,16 +16,6 @@ ostream &operator<<(ostream &os, const T_container &v)
 }
 
 #define ll long long
-#define fi first
-#define se second
-
-#define len(x) (int)(x).size()
-
-typedef vector<int> vi;
-typedef vector<vi> vii;
-typedef pair<int, int> pii;
-
-const ll MOD = 1e9 + 7;
 
 void solve()
 {
@@ -35,13 +26,11 @@ int main(int argc, char *argv[])
     if (argc >= 2)
         freopen(argv[1], "r", stdin);
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(nullptr);
     int tc = 1;
     cin >> tc;
-    for (int t = 1; t <= tc; t++)
+    while (tc--)
     {
-        // cout << "Case #" << t << "\n";
         solve();
     }
 }
