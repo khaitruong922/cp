5c199ae2f7c5240ba5f6a47734fcd90f0b46805b
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-10-08 16:19:11 +0700

    Create sqrt.cpp

---
+++ template/sqrt.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll lsqrt(ll x)
+{
+    ll ans = 0;
+    for (ll k = 1LL << 30; k != 0; k /= 2)
+    {
+        if ((ans + k) * (ans + k) <= x)
+        {
+            ans += k;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    cout << lsqrt(0) << endl;
+    cout << lsqrt(2) << endl;
+    cout << lsqrt(4) << endl;
+    cout << lsqrt(1e3) << endl;
+    cout << lsqrt(1e17) << endl;
+    cout << lsqrt(LLONG_MAX) << endl;
+}
