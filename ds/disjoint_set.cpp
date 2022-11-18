659d770f35d893e510f41e065d498af6ef068e60
Khai Truong <khai.truong@codelink.io>
2022-11-19 00:59:09 +0700

    upd dsu

--- ds/disjoint_set.cpp
+++ ds/disjoint_set.cpp
@@ -1,85 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-template <typename A, typename B>
-ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
-ostream &operator<<(ostream &os, const T_container &v)
+const int N = 2e5 + 5;
+int p[N];
+
+void init()
 {
-    os << '{';
-    string sep;
-    for (const T &x : v)
-        os << sep << x, sep = ", ";
-    return os << '}';
+    memset(p, -1, sizeof p);
 }
 
-#define ar array
-#define ll long long
-#define ld long double
-#define pb push_back
-#define ppb pop_back
-#define fi first
-#define se second
-
-#define sza(x) ((int)x.size())
-#define all(a) (a).begin(), (a).end()
-
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<string> vs;
-typedef pair<int, int> pii;
-
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
-const ld EPS = 1e-9;
-
-const int MAX_N = 2e5 + 5;
-ll a[MAX_N];
-
-int find(int i, vector<int> &parent)
+int find(int u)
 {
-    if (parent[i] < 0)
-        return i;
-    return parent[i] = find(parent[i], parent);
+    if (p[u] < 0)
+        return u;
+    return p[u] = find(p[u]);
 }
 
-void solve()
+void uni(int u, int v)
 {
-    int n = 8;
-    vector<pii> paths{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {2, 4}, {2, 5}, {1, 3}, {6, 8}, {5, 7}};
-    vector<int> parent(n + 1, -1);
-    cout << parent << '\n';
-    for (auto &p : paths)
-    {
-        int u = find(p.fi, parent);
-        int v = find(p.se, parent);
-
-        if (u == v)
-            continue;
-        if (parent[u] > parent[v])
-            swap(u, v);
-        parent[u] += parent[v];
-        parent[v] = u;
-    }
-    cout << parent << endl;
-    for (int i = 1; i <= n; i++)
-        if (parent[i] > 0)
-            parent[i] = find(parent[i], parent);
-    cout << parent << endl;
+    u = find(u), v = find(v);
+    if (u == v)
+        return;
+    if (p[u] > p[v])
+        swap(u, v);
+    p[u] += p[v];
+    p[v] = u;
 }
 
-int main(int argc, char *argv[])
+bool check(int u, int v)
 {
-    if (argc >= 2)
-        freopen(argv[1], "r", stdin);
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int tc = 1;
-    // cin >> tc;
-    for (int t = 1; t <= tc; t++)
-    {
-        solve();
-    }
+    return find(u) == find(v);
 }
