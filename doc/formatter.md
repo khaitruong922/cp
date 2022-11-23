70838c15eefc4a48868ad24ce6bc1f0f3689b137
Khai Truong <khai.truong@codelink.io>
2022-11-23 13:35:26 +0700

    ok

---
+++ doc/formatter.md
@@ -0,0 +1,2 @@
+C_Cpp: Clang_format_style
+{ BasedOnStyle: Google, IndentWidth: 4 }
--- template.cpp
+++ template.cpp
@@ -12,20 +12,15 @@ using pii = pair<int, int>;
 
 const int P = 1e9 + 7;
 
-void solve()
-{
-}
+void solve() {}
 
-int main(int argc, char *argv[])
-{
-    if (argc >= 2)
-        freopen(argv[1], "r", stdin);
+int main(int argc, char *argv[]) {
+    if (argc >= 2) freopen(argv[1], "r", stdin);
     ios_base::sync_with_stdio(0);
     cin.tie(nullptr);
     int tc = 1;
     cin >> tc;
-    while (tc--)
-    {
+    while (tc--) {
         solve();
     }
 }
